70abfa48684c504b5ff20c3fbdf946c139cb3ccc
J Scott Smith <jscsmith@gmail.com>
2017-03-14 00:35:38 -0700

    cleanup server

--- examples/server.js
+++ examples/server.js
@@ -12,12 +12,12 @@ app.use('/static', express.static(path.resolve(__dirname, '../dist')));
 app.use('/static', express.static(__dirname));
 
 app.get('*', (req, res) => {
-  const html = fs.readFileSync(path.resolve(__dirname, './index.html')).toString();
-  const markup = ReactServer.renderToString(<HelloWorld />);
+    const html = fs.readFileSync(path.resolve(__dirname, './index.html')).toString();
+    const markup = ReactServer.renderToString(<HelloWorld />);
 
-  res.send(html.replace('$react', markup));
+    res.send(html.replace('$react', markup));
 });
 
 app.listen(3000, () => {
-  console.log('Listening on: http://localhost:3000')
+    console.log('Listening on: http://localhost:3000')
 });
