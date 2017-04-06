a31c6f6effe767a63b77071c322ddddaa9eb574c
J Scott Smith <jscsmith@gmail.com>
2017-04-06 00:20:37 -0700

    lint webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,77 +7,77 @@ const nodeExternals = require('webpack-node-externals');
 const MinifierPlugin = webpack.optimize.UglifyJsPlugin;
 
 const clientConfig = {
-  entry: path.resolve('./examples/client.js'),
-  output: {
-    path: path.resolve('./dist'),
-    filename: 'bundle.js',
-  },
-
-  plugins: [
-    PRODUCTION && new MinifierPlugin(),
-  ].filter(e => e),
-
-  resolve: {
-    alias: {
-      'react-scroll-parallax': path.resolve('./src/'),
+    entry: path.resolve('./examples/client.js'),
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'bundle.js',
     },
-  },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [
-          path.resolve('./src'),
-          path.resolve('./examples'),
+    plugins: [
+        PRODUCTION && new MinifierPlugin(),
+    ].filter(e => e),
+
+    resolve: {
+        alias: {
+            'react-scroll-parallax': path.resolve('./src/'),
+        },
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.resolve('./src'),
+                    path.resolve('./examples'),
+                ],
+                loader: 'babel-loader',
+                query: createBabelConfig(),
+            },
         ],
-        loader: 'babel-loader',
-        query: createBabelConfig(),
-      }
-    ],
-  },
+    },
 };
 
 const serverConfig = {
-  target: 'node',
+    target: 'node',
 
-  watch: true,
+    watch: true,
 
-  externals: [ nodeExternals() ],
+    externals: [nodeExternals()],
 
-  node: {
-    __dirname: true
-  },
-
-  entry: path.resolve('./examples/server.js'),
-  output: {
-    path: path.resolve('./dist'),
-    filename: 'server.js',
-  },
-
-  plugins: [
-    PRODUCTION && new MinifierPlugin(),
-  ].filter(e => e),
-
-  resolve: {
-    alias: {
-      'react-scroll-parallax': path.resolve('./src/'),
+    node: {
+        __dirname: true,
     },
-  },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [
-          path.resolve('./src'),
-          path.resolve('./examples'),
+    entry: path.resolve('./examples/server.js'),
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'server.js',
+    },
+
+    plugins: [
+        PRODUCTION && new MinifierPlugin(),
+    ].filter(e => e),
+
+    resolve: {
+        alias: {
+            'react-scroll-parallax': path.resolve('./src/'),
+        },
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.resolve('./src'),
+                    path.resolve('./examples'),
+                ],
+                loader: 'babel-loader',
+                query: createBabelConfig({ server: true }),
+            },
         ],
-        loader: 'babel-loader',
-        query: createBabelConfig({ server: true }),
-      }
-    ],
-  },
+    },
 };
 
 module.exports = [clientConfig, serverConfig];
