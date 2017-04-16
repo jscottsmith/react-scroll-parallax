e8c02dd8f190bad8a3590a945eaceb74fa422ea0
J Scott Smith <jscsmith@gmail.com>
2017-04-15 18:35:07 -0700

    add webpack loaders for handing sass as css modules, add raw loader for svg

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ const clientConfig = {
     resolve: {
         alias: {
             'react-scroll-parallax': path.resolve('./src/'),
+            'components': path.resolve('./examples/components/'),
         },
     },
 
@@ -33,6 +34,36 @@ const clientConfig = {
                 ],
                 loader: 'babel-loader',
                 query: createBabelConfig(),
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|cur)$/,
+                loader: 'url-loader',
+                include: [
+                    path.resolve('./src'),
+                ],
+            },
+            {
+                test: /\.scss$/,
+                include: [
+                    path.resolve('./examples'),
+                ],
+                loaders: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        query: {
+                            localIdentName: '[name]_[local]_[hash:base64:3]',
+                            importLoaders: 1,
+                        },
+                    },
+                    'postcss-loader',
+                    {
+                        loader: 'sass-loader',
+                        query: {
+                            outputStyle: 'expanded',
+                        },
+                    },
+                ],
             },
         ],
     },
@@ -62,6 +93,7 @@ const serverConfig = {
     resolve: {
         alias: {
             'react-scroll-parallax': path.resolve('./src/'),
+            'components': path.resolve('./examples/components/'),
         },
     },
 
@@ -75,6 +107,27 @@ const serverConfig = {
                 ],
                 loader: 'babel-loader',
                 query: createBabelConfig({ server: true }),
+            },
+            {
+                test: /\.scss$/,
+                include: [
+                    path.resolve('./examples'),
+                ],
+                loaders: [
+                    {
+                        loader: 'css-loader/locals',
+                        query: {
+                            localIdentName: '[name]_[local]_[hash:base64:3]',
+                            sourceMap: false,
+                        },
+                    },
+                    'postcss-loader', {
+                        loader: 'sass-loader',
+                        query: {
+                            outputStyle: 'expanded',
+                        },
+                    },
+                ],
             },
         ],
     },
