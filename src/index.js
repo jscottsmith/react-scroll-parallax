b00460ae11f9f8d25c4880860074f0b75f10aa39
J Scott Smith <jscsmith@gmail.com>
2017-08-26 14:59:13 -0700

    remove ScrollPosition from src

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export ScrollPosition from './components/ScrollPosition';
 export ScrollProvider from './components/ScrollProvider';
 export ScrollController from './libs/ScrollController';
 export withScrollPosition from './hoc/withScrollPosition.js';
