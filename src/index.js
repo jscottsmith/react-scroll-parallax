155804a0a2177f021bf8a6b17a1879387673b4de
J Scott Smith <jscsmith@gmail.com>
2017-08-27 14:02:41 -0700

    export `withObserver` from src index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export ScrollProvider from './components/ScrollProvider';
 export ScrollController from './libs/ScrollController';
 export withScrollPosition from './hoc/withScrollPosition.js';
+export withObserver from './hoc/withObserver.js';
