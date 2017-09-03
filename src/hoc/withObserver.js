c8f6b4bad52f20d563d3e1aaf639c2a33d81c233
J Scott Smith <jscsmith@gmail.com>
2017-09-03 15:44:31 -0700

    use `getRef` prop func in the withObserver hoc

--- src/hoc/withObserver.js
+++ src/hoc/withObserver.js
@@ -1,20 +1,25 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 
 // With Observer creates a new IntersectionObserver
 // that passes an `isInView` prop to wrapped components
 // IntersectionObserver will require a polyfill
 
-export default function withObserver(
+function withObserver(
     WrappedComponent,
     options = {
         root: null, // document.querySelector('#scrollArea')
-        rootMargin: '0px',
-        threshold: [0.1, 1.0], // 1.0 = 100% of the el is visible, can be an array of values. callback is invoke when each is true
+        rootMargin: '0px', // this needs to be set based on the offsets
+        threshold: [0, 0.2, 0.4, 0.6, 0.8, 1.0], // 1.0 = 100% of the el is visible, can be an array of values. callback is invoke when each is true
     }
 ) {
     return class Observed extends PureComponent {
+        static propTypes = {
+            getRef: PropTypes.func.isRequired,
+        };
+
         state = {
-            isInView: null,
+            isInView: true, // initial state should be visible so that the scrollY is updated.
         };
 
         componentDidMount() {
@@ -38,28 +43,26 @@ export default function withObserver(
         }
 
         observe() {
-            this.observer.observe(this.el);
+            // NOTE: gets the ref of the element to observe
+            const el = this.props.getRef();
+
+            this.observer.observe(el);
         }
 
         handleIntersection = (entries, observer) => {
             entries.forEach(entry => {
-                const isInView = entry.intersectionRatio > 0;
+                const isInView = entry.intersectionRatio > 0.01;
 
                 this.setState(() => ({ isInView }));
             });
         };
 
-        mapRef = ref => {
-            this.el = ref;
-        };
-
         render() {
             const { isInView } = this.state;
-            return (
-                <div ref={this.mapRef}>
-                    <WrappedComponent isInView={isInView} {...this.props} />
-                </div>
-            );
+
+            return <WrappedComponent isInView={isInView} {...this.props} />;
         }
     };
 }
+
+export default withObserver;
