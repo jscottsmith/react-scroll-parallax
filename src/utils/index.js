0156c990cc8cf443759bc81893bf57e26100cc11
J Scott Smith <jscsmith@gmail.com>
2017-09-03 22:13:42 -0700

    remove unused utils and tests

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,4 @@
 export clamp from './clamp';
 export compose from './compose';
-export getParallaxOffsets from './getParallaxOffsets';
-export isElementInView from './isElementInView';
-export parseValueAndUnit from './parseValueAndUnit';
 export scaleBetween from './scaleBetween';
 export testForPassiveScroll from './testForPassiveScroll';
