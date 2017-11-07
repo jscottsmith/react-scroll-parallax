2eb830947e385b21e87dce4f47fded0c0c812d11
J Smith <jscsmith@gmail.com>
2017-11-07 10:43:09 -0800

    spaces

--- __tests__/parseUnit.test.js
+++ __tests__/parseUnit.test.js
@@ -1,12 +1,12 @@
 import parseUnit from 'utils/parseUnit.js';
 
 test('Parses a value and returns an object with the value and unit', () => {
-	expect(parseUnit('3450px')).toMatchObject({ unit: 'px', value: 3450 });
-	expect(parseUnit('1000')).toMatchObject({ unit: 'px', value: 1000 });
-	expect(parseUnit(980)).toMatchObject({ unit: 'px', value: 980 });
-	expect(parseUnit('86%')).toMatchObject({ unit: '%', value: 86 });
-	expect(parseUnit('1283fubar')).toMatchObject({
-		unit: 'fubar',
-		value: 1283,
-	});
+    expect(parseUnit('3450px')).toMatchObject({ unit: 'px', value: 3450 });
+    expect(parseUnit('1000')).toMatchObject({ unit: 'px', value: 1000 });
+    expect(parseUnit(980)).toMatchObject({ unit: 'px', value: 980 });
+    expect(parseUnit('86%')).toMatchObject({ unit: '%', value: 86 });
+    expect(parseUnit('1283fubar')).toMatchObject({
+        unit: 'fubar',
+        value: 1283,
+    });
 });
