796a6959ef9c9b8b1f0b58c542dce9aebfb9388c
J Smith <jscsmith@gmail.com>
2017-11-12 18:21:56 -0800

    move x, y, and scale transforms to functions

--- src/utils/createBoundsStyle.js
+++ src/utils/createBoundsStyle.js
@@ -4,24 +4,23 @@ export default function createBoundsStyle(x, y, scale, el) {
     // It takes the x, y, and scale props then resizes
     // provides the styles to create the bounds.
 
-    // @TODO: Consider revising this function.
-    // Break into multiple focused functions
-    // for simpler testing
-
     // NOTE: Temporary border added to debug
     const border = '1px dotted tomato';
 
+    // for percents and scale
     let height = 0;
     let width = 0;
 
-    let mt = 0;
-    let mb = 0;
-    let ml = 0;
-    let mr = 0;
-    let pt = 0;
-    let pb = 0;
-    let pl = 0;
-    let pr = 0;
+    let bounds = {};
+
+    bounds.mt = 0;
+    bounds.mb = 0;
+    bounds.ml = 0;
+    bounds.mr = 0;
+    bounds.pt = 0;
+    bounds.pb = 0;
+    bounds.pl = 0;
+    bounds.pr = 0;
 
     const hasScale = scale;
     const hasYPercent = y[0].unit === '%';
@@ -47,63 +46,83 @@ export default function createBoundsStyle(x, y, scale, el) {
     }
 
     if (y) {
-        const y0 = y[0].value;
-        const y1 = y[1].value;
-
-        // transform percent to px
-        const value0 = hasYPercent ? y0 / 100 * height : y0;
-        const value1 = hasYPercent ? y1 / 100 * height : y1;
-
-        const yMin = Math.min(value0, value1);
-        const yMax = Math.max(value0, value1);
-
-        mt = mt + yMin;
-        mb = mb - yMax;
-        pt = pt + yMin * -1;
-        pb = pb + yMax;
+        bounds = transformBoundsY(bounds, y, height);
     }
 
     if (x) {
-        const x0 = x[0].value;
-        const x1 = x[1].value;
-
-        // transform percent to px
-        const value0 = hasXPercent ? x0 / 100 * width : x0;
-        const value1 = hasXPercent ? x1 / 100 * width : x1;
-
-        const xMin = Math.min(value0, value1);
-        const xMax = Math.max(value0, value1);
-
-        ml = ml + xMin;
-        mr = mr - xMax;
-        pl = pl + xMin * -1;
-        pr = pr + xMax;
+        bounds = transformBoundsX(bounds, x, width);
     }
 
     if (scale) {
-        const scaleMax = Math.max(scale[0], scale[1]);
-        const halfDeltaX = (width * scaleMax - width) / 2;
-        const halfDeltaY = (height * scaleMax - height) / 2;
-
-        mt = mt - halfDeltaY;
-        mb = mb - halfDeltaY;
-        ml = ml - halfDeltaX;
-        mr = mr - halfDeltaX;
-        pt = mt * -1;
-        pb = mb * -1;
-        pl = ml * -1;
-        pr = mr * -1;
+        bounds = transformBoundsScale(bounds, scale, width, height);
     }
 
     return {
-        marginTop: mt,
-        marginBottom: mb,
-        marginLeft: ml,
-        marginRight: mr,
-        paddingTop: pt,
-        paddingBottom: pb,
-        paddingLeft: pl,
-        paddingRight: pr,
+        marginTop: bounds.mt,
+        marginBottom: bounds.mb,
+        marginLeft: bounds.ml,
+        marginRight: bounds.mr,
+        paddingTop: bounds.mt * -1,
+        paddingBottom: bounds.mb * -1,
+        paddingLeft: bounds.ml * -1,
+        paddingRight: bounds.mr * -1,
         border,
     };
+}
+
+export function transformBoundsY(bounds, y, h) {
+    const newBounds = Object.assign({}, bounds);
+
+    const hasYPercent = y[0].unit === '%';
+
+    const y0 = y[0].value;
+    const y1 = y[1].value;
+
+    // transform percent to px
+    const value0 = hasYPercent ? y0 / 100 * h : y0;
+    const value1 = hasYPercent ? y1 / 100 * h : y1;
+
+    const yMin = Math.min(value0, value1);
+    const yMax = Math.max(value0, value1);
+
+    newBounds.mt = newBounds.mt + yMin;
+    newBounds.mb = newBounds.mb - yMax;
+
+    return newBounds;
+}
+
+export function transformBoundsX(bounds, x, w) {
+    const newBounds = Object.assign({}, bounds);
+
+    const hasXPercent = x[0].unit === '%';
+
+    const x0 = x[0].value;
+    const x1 = x[1].value;
+
+    // transform percent to px
+    const value0 = hasXPercent ? x0 / 100 * w : x0;
+    const value1 = hasXPercent ? x1 / 100 * w : x1;
+
+    const xMin = Math.min(value0, value1);
+    const xMax = Math.max(value0, value1);
+
+    newBounds.ml = newBounds.ml + xMin;
+    newBounds.mr = newBounds.mr - xMax;
+
+    return newBounds;
+}
+
+export function transformBoundsScale(bounds, scale, w, h) {
+    const newBounds = Object.assign({}, bounds);
+
+    const scaleMax = Math.max(scale[0], scale[1]);
+    const halfDeltaX = (w * scaleMax - w) / 2;
+    const halfDeltaY = (h * scaleMax - h) / 2;
+
+    newBounds.mt = newBounds.mt - halfDeltaY;
+    newBounds.mb = newBounds.mb - halfDeltaY;
+    newBounds.ml = newBounds.ml - halfDeltaX;
+    newBounds.mr = newBounds.mr - halfDeltaX;
+
+    return newBounds;
 }
