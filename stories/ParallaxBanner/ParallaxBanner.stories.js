4b07d8a159ea7b4ce37061ae2ca3158a70ea6929
J Smith <jscsmith@gmail.com>
2018-03-04 17:58:58 -0800

    add video and custom layer children stories #22

--- stories/ParallaxBanner/ParallaxBanner.stories.js
+++ stories/ParallaxBanner/ParallaxBanner.stories.js
@@ -81,6 +81,72 @@ storiesOf('<ParallaxBanner>', module)
             />
         );
     })
+    .add('with a video', () => (
+        <ParallaxBanner
+            className={styles.bannerBg}
+            layers={[
+                {
+                    amount: 0.3,
+                    children: (
+                        <video
+                            className={styles.video}
+                            autoPlay
+                            loop
+                            playsInline
+                            preload="auto"
+                            muted
+                            poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/105988/boats-at-sea.jpg"
+                            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/105988/boats-at-sea-720.mp4"
+                        />
+                    ),
+                    slowerScrollRate: true,
+                },
+            ]}
+        >
+            <div className={styles.parallaxChildren}>
+                <h1>Video Background</h1>
+            </div>
+        </ParallaxBanner>
+    ))
+    .add('with custom layer children', () => (
+        <ParallaxBanner
+            className={styles.bannerBg}
+            layers={[
+                {
+                    children: (
+                        <div className={`${styles.customChild} ${styles.red}`}>
+                            Red
+                        </div>
+                    ),
+                    amount: 0.2,
+                    expanded: false,
+                    slowerScrollRate: false,
+                },
+                {
+                    children: (
+                        <div
+                            className={`${styles.customChild} ${styles.green}`}
+                        >
+                            Green
+                        </div>
+                    ),
+                    amount: 0.4,
+                    expanded: false,
+                    slowerScrollRate: false,
+                },
+                {
+                    children: (
+                        <div className={`${styles.customChild} ${styles.blue}`}>
+                            Blue
+                        </div>
+                    ),
+                    amount: 0.6,
+                    expanded: false,
+                    slowerScrollRate: false,
+                },
+            ]}
+        />
+    ))
     .add('with parallax disabled', () => (
         <ParallaxBanner
             className={styles.bannerBg}
