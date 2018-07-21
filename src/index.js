c48f82508e40c0033851536da2c6618cf364438f
J Smith <jscsmith@gmail.com>
2018-07-21 13:01:49 -0700

    #36 test and readme updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+export withController from './components/withController';
 export Parallax from './components/Parallax';
 export ParallaxProvider from './components/ParallaxProvider';
 export ParallaxBanner from './components/ParallaxBanner';
+export withController from './components/withController';
 export ParallaxController from './modules/ParallaxController';
