ea4b5a13b0ef8872c58e95352e0fedcfa4b27895
J Smith <jscsmith@gmail.com>
2019-01-27 13:53:45 -0800

    fix export

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ export withController from './components/withController';
 export Parallax from './components/Parallax';
 export ParallaxProvider from './components/ParallaxProvider';
 export ParallaxBanner from './components/ParallaxBanner';
-export withController from './components/withController';
 export ParallaxController from './modules/ParallaxController';
