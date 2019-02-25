32f0b5db250031743f6022a69420572ff02a9a05
J Smith <jscsmith@gmail.com>
2019-02-24 23:05:40 -0800

    add getCache tests

--- __tests__/testUtils/createElementMock.js
+++ __tests__/testUtils/createElementMock.js
@@ -1,12 +1,15 @@
-export function createElementMock() {
-    const elOuter = document.createElement('div');
-    Object.defineProperty(elOuter, 'offsetHeight', {
-        value: 120,
-        writable: false,
+export function createElementMock(properties = {}, methods = {}) {
+    const element = document.createElement('div');
+    Object.keys(properties).map(key => {
+        Object.defineProperty(element, key, {
+            value: properties[key],
+            writable: false,
+        });
     });
-    Object.defineProperty(elOuter, 'offsetWidth', {
-        value: 120,
-        writable: false,
+
+    Object.keys(methods).map(key => {
+        element[key] = methods[key];
     });
-    return elOuter;
+
+    return element;
 }
