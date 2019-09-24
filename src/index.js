bc9e48a0751264a341281ed55a0bde6576f34e05
zalishchuk <zalishchuk.me@gmail.com>
2019-09-24 04:24:36 +0300

    Export context for hooks support

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export withController from './components/withController';
 export Parallax from './components/Parallax';
 export ParallaxProvider from './components/ParallaxProvider';
 export ParallaxBanner from './components/ParallaxBanner';
+export ParallaxContext from './helpers/ParallaxContext';
