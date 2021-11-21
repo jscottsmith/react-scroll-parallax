484610feac9119743f230c01c7200749791a7ced
J Scott Smith <jscsmith@gmail.com>
2021-11-21 15:57:54 -0800

    update stories

--- stories/Image/Image.js
+++ stories/Image/Image.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import * as style from './Image.module.scss';
+import styles from './Image.module.scss';
 import { useController } from '../../src';
 
-export const Image = () => {
-  const parallaxController = useController();
-  function handleLoad() {
-    parallaxController.update();
-  }
-  return (
-    <div className={style.image}>
-      <img src={props.src} onLoad={handleLoad} />
-    </div>
-  );
+export const Image = (props) => {
+    const parallaxController = useController();
+    function handleLoad() {
+        parallaxController.update();
+    }
+    return (
+        <div className={styles.image}>
+            <img src={props.src} onLoad={handleLoad} />
+        </div>
+    );
 };
 
 export default Image;
