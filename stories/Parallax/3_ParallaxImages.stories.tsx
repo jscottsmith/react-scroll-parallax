28dd0df4b22bc407812179c213ac6bd9e3466fdf
J Scott Smith <jscsmith@gmail.com>
2021-11-28 13:43:53 -0800

    fix parallax image stories

--- stories/Parallax/3_ParallaxImages.stories.tsx
+++ stories/Parallax/3_ParallaxImages.stories.tsx
@@ -16,22 +16,22 @@ export const WithVaryingYOffsets = () => {
   return (
     <Container scrollAxis="vertical">
       <div className={styles.images}>
-        <Parallax y={[20, -20]} className={styles.parallaxImage}>
+        <Parallax speed={4} className={styles.parallaxImage}>
           <Image src={imageFile1} />
         </Parallax>
-        <Parallax y={[-20, 20]} className={styles.parallaxImage}>
+        <Parallax speed={-4} className={styles.parallaxImage}>
           <Image src={imageFile2} />
         </Parallax>
-        <Parallax y={[20, -20]} className={styles.parallaxImage2}>
+        <Parallax speed={4} className={styles.parallaxImage2}>
           <Image src={imageFile4} />
         </Parallax>
-        <Parallax y={[-10, 10]} className={styles.parallaxImage}>
+        <Parallax speed={-3} className={styles.parallaxImage}>
           <Image src={imageFile5} />
         </Parallax>
-        <Parallax y={[10, -10]} className={styles.parallaxImage}>
+        <Parallax speed={3} className={styles.parallaxImage}>
           <Image src={imageFile3} />
         </Parallax>
-        <Parallax y={[30, -30]} className={styles.parallaxImage2}>
+        <Parallax speed={6} className={styles.parallaxImage2}>
           <Image src={imageFile6} />
         </Parallax>
       </div>
