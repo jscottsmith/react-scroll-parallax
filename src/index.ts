da7f48dfff8e21291166cabd569765535e91f7a2
J Scott Smith <jscsmith@gmail.com>
2022-01-01 23:51:34 -0800

    fix ts errors in tests

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
+import { EasingPreset, EffectNumber, EffectString } from 'parallax-controller';
+
 import { Parallax } from './components/Parallax';
 import { ParallaxBanner } from './components/ParallaxBanner';
 import { ParallaxProvider } from './components/ParallaxProvider';
 import { ParallaxContext } from './context/ParallaxContext';
 export { useController } from './hooks/useController';
-export { Parallax, ParallaxBanner, ParallaxProvider, ParallaxContext };
+export {
+  Parallax,
+  ParallaxBanner,
+  ParallaxProvider,
+  ParallaxContext,
+  EasingPreset,
+  EffectNumber,
+  EffectString,
+};
