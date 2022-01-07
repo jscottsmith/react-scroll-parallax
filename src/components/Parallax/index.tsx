1865ada7a0a94f919d7e9fe836e5549d47dbe928
J Scott Smith <jscsmith@gmail.com>
2022-01-07 00:33:39 -0800

    add `useParallax` hook

--- src/components/Parallax/index.tsx
+++ src/components/Parallax/index.tsx
@@ -1,126 +1,16 @@
-import React, { PropsWithChildren, useEffect, useRef, useState } from 'react';
-import { CreateElementOptions, Element } from 'parallax-controller';
-import { useController } from '../../hooks/useController';
-import { removeUndefinedObjectKeys } from '../../utils/removeUndefinedObjectKeys';
-import { ParallaxProps } from './types';
-import { useVerifyController } from './hooks';
+import React, { PropsWithChildren } from 'react';
+import { useParallax } from '../../hooks/useParallax';
+import { ParallaxProps } from '../../types';
 
 export function Parallax(props: PropsWithChildren<ParallaxProps>) {
-  const controller = useController();
-  const refInner = useRef<HTMLElement>();
-
-  useVerifyController(controller);
-
-  function _getElementOptions(): CreateElementOptions {
-    const useSpeedProp = typeof props.speed !== 'undefined';
-    const isHorizontal = controller?.scrollAxis == 'horizontal';
-    const isVertical = controller?.scrollAxis == 'vertical';
-
-    let translateX = props.translateX;
-    let translateY = props.translateY;
-
-    if (useSpeedProp && isHorizontal) {
-      translateX = [
-        `${(props.speed || 0) * 10}px`,
-        `${(props.speed || 0) * -10}px`,
-      ];
-    }
-
-    if (useSpeedProp && isVertical) {
-      translateY = [
-        `${(props.speed || 0) * 10}px`,
-        `${(props.speed || 0) * -10}px`,
-      ];
-    }
-
-    return {
-      // @ts-expect-error
-      el: refInner.current,
-      props: removeUndefinedObjectKeys({
-        translateX,
-        translateY,
-        disabled: props.disabled,
-        rotate: props.rotate,
-        rotateX: props.rotateX,
-        rotateY: props.rotateY,
-        rotateZ: props.rotateZ,
-        scale: props.scale,
-        scaleX: props.scaleX,
-        scaleY: props.scaleY,
-        scaleZ: props.scaleZ,
-        opacity: props.opacity,
-        easing: props.easing,
-        rootMargin: props.rootMargin,
-        shouldStartAnimationInitialInView:
-          props.shouldStartAnimationInitialInView,
-        onProgressChange: props.onProgressChange,
-        onChange: props.onChange,
-        onEnter: props.onEnter,
-        onExit: props.onExit,
-        startScroll: props.startScroll,
-        endScroll: props.endScroll,
-        targetElement: props.targetElement,
-      }),
-    };
-  }
-
-  const [element, setElement] = useState<Element>();
-
-  // create element
-  useEffect(() => {
-    const newElement = controller?.createElement(_getElementOptions());
-    setElement(newElement);
-
-    return () => {
-      if (newElement) {
-        controller?.removeElementById(newElement.id);
-      }
-    };
-  }, []);
-
-  // update element
-  useEffect(() => {
-    if (element) {
-      if (props.disabled) {
-        controller?.resetElementStyles(element);
-      } else {
-        controller?.updateElementPropsById(
-          element.id,
-          _getElementOptions().props
-        );
-      }
-    }
-  }, [
-    props.disabled,
-    props.translateX,
-    props.translateY,
-    props.rotate,
-    props.rotateX,
-    props.rotateY,
-    props.rotateZ,
-    props.scale,
-    props.scaleX,
-    props.scaleY,
-    props.scaleZ,
-    props.speed,
-    props.opacity,
-    props.easing,
-    props.rootMargin,
-    props.onProgressChange,
-    props.onChange,
-    props.onEnter,
-    props.onExit,
-    props.targetElement,
-  ]);
-
   const Outer = props.tag;
   const Inner = props.innerTag;
-
+  const { ref } = useParallax(props);
   return (
     <Outer className={props.className} style={props.style}>
       <Inner
         className={props.innerClassName}
-        ref={refInner}
+        ref={ref}
         style={props.innerStyle}
       >
         {props.children}
---
+++ src/hooks/useParallax.ts
@@ -0,0 +1,119 @@
+import { CreateElementOptions, Element } from 'parallax-controller';
+import { useEffect, useRef, useState } from 'react';
+import { useVerifyController } from '../components/Parallax/hooks';
+import { ParallaxProps } from '../types';
+import { removeUndefinedObjectKeys } from '../utils/removeUndefinedObjectKeys';
+import { useController } from './useController';
+
+export function useParallax(props: ParallaxProps) {
+  const controller = useController();
+  const ref = useRef<HTMLElement>();
+
+  useVerifyController(controller);
+
+  function _getElementOptions(): CreateElementOptions {
+    const useSpeedProp = typeof props.speed !== 'undefined';
+    const isHorizontal = controller?.scrollAxis == 'horizontal';
+    const isVertical = controller?.scrollAxis == 'vertical';
+
+    let translateX = props.translateX;
+    let translateY = props.translateY;
+
+    // TODO: move to parallax-controller
+    if (useSpeedProp && isHorizontal) {
+      translateX = [
+        `${(props.speed || 0) * 10}px`,
+        `${(props.speed || 0) * -10}px`,
+      ];
+    }
+
+    if (useSpeedProp && isVertical) {
+      translateY = [
+        `${(props.speed || 0) * 10}px`,
+        `${(props.speed || 0) * -10}px`,
+      ];
+    }
+
+    return {
+      // @ts-expect-error
+      el: ref.current,
+      props: removeUndefinedObjectKeys({
+        translateX,
+        translateY,
+        disabled: props.disabled,
+        rotate: props.rotate,
+        rotateX: props.rotateX,
+        rotateY: props.rotateY,
+        rotateZ: props.rotateZ,
+        scale: props.scale,
+        scaleX: props.scaleX,
+        scaleY: props.scaleY,
+        scaleZ: props.scaleZ,
+        opacity: props.opacity,
+        easing: props.easing,
+        rootMargin: props.rootMargin,
+        shouldStartAnimationInitialInView:
+          props.shouldStartAnimationInitialInView,
+        onProgressChange: props.onProgressChange,
+        onChange: props.onChange,
+        onEnter: props.onEnter,
+        onExit: props.onExit,
+        startScroll: props.startScroll,
+        endScroll: props.endScroll,
+        targetElement: props.targetElement,
+      }),
+    };
+  }
+
+  const [element, setElement] = useState<Element>();
+
+  // create element
+  useEffect(() => {
+    const newElement = controller?.createElement(_getElementOptions());
+
+    setElement(newElement);
+
+    return () => {
+      if (newElement) {
+        controller?.removeElementById(newElement.id);
+      }
+    };
+  }, []);
+
+  // update element
+  useEffect(() => {
+    if (element) {
+      if (props.disabled) {
+        controller?.resetElementStyles(element);
+      } else {
+        controller?.updateElementPropsById(
+          element.id,
+          _getElementOptions().props
+        );
+      }
+    }
+  }, [
+    props.disabled,
+    props.translateX,
+    props.translateY,
+    props.rotate,
+    props.rotateX,
+    props.rotateY,
+    props.rotateZ,
+    props.scale,
+    props.scaleX,
+    props.scaleY,
+    props.scaleZ,
+    props.speed,
+    props.opacity,
+    props.easing,
+    props.rootMargin,
+    props.onProgressChange,
+    props.onChange,
+    props.onEnter,
+    props.onExit,
+    props.targetElement,
+  ]);
+
+  return { ref };
+}
