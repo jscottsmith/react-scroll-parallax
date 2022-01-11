bb03d1ab648d5d9f7fd78b5da18ed91c1df7fc30
J Scott Smith <jscsmith@gmail.com>
2022-01-11 15:37:39 -0800

    feat: return a single element from <Parallax>, remove unused inner element props

--- src/components/Parallax/index.tsx
+++ src/components/Parallax/index.tsx
@@ -3,33 +3,16 @@ import { useParallax } from '../../hooks/useParallax';
 import { ParallaxProps } from '../../types';
 
 export function Parallax(props: PropsWithChildren<ParallaxProps>) {
-  const Outer = props.tag;
-  const Inner = props.innerTag;
+  const Tag = props.tag;
   const { ref } = useParallax(props);
   return (
-    <Outer className={props.className} style={props.style}>
-      <Inner
-        className={props.innerClassName}
-        ref={ref}
-        style={props.innerStyle}
-      >
-        {props.children}
-      </Inner>
-    </Outer>
+    <Tag className={props.className} style={props.style} ref={ref}>
+      {props.children}
+    </Tag>
   );
 }
 
-export const Component = () => {
-  const { ref } = useParallax<HTMLDivElement>({ translateY: [100, -100] });
-  return (
-    <div ref={ref}>
-      <div />
-    </div>
-  );
-};
-
 Parallax.defaultProps = {
   disabled: false,
-  innerTag: 'div',
   tag: 'div',
 };
