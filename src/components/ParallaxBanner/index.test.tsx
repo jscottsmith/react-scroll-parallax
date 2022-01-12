2318e961d9391b6d696b13180ece60288ff2e05a
J Scott Smith <jscsmith@gmail.com>
2022-01-12 14:56:12 -0800

    test: update ParallaxBanner tests using react-testing-library

--- src/components/ParallaxBanner/index.test.tsx
+++ src/components/ParallaxBanner/index.test.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// @ts-ignore
-import renderer from 'react-test-renderer';
-import createNodeMock from '../../testUtils/createNodeMock';
+import { render } from '@testing-library/react';
 import { ParallaxBanner } from '.';
 import { ParallaxProvider } from '../ParallaxProvider';
 
-describe('Expect the <ParallaxBanner> component', () => {
-  afterEach(() => {});
-
-  it('to render image banners correctly', () => {
-    const tree = renderer
-      .create(
+describe('given a <ParallaxBanner> component', () => {
+  describe('with all props', () => {
+    it('then it will render banners correctly', () => {
+      const { asFragment } = render(
         <ParallaxProvider>
           <ParallaxBanner
             className="test-class"
@@ -31,114 +26,94 @@ describe('Expect the <ParallaxBanner> component', () => {
               <h1>Foo Bar</h1>
             </div>
           </ParallaxBanner>
-        </ParallaxProvider>,
-        {
-          createNodeMock,
-        }
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+        </ParallaxProvider>
+      );
+      expect(asFragment()).toMatchSnapshot();
+    });
   });
 
-  it('to render custom child banners correctly', () => {
-    const tree = renderer
-      .create(
+  describe('with custom defined layer children', () => {
+    it('then it will render each layer child', () => {
+      const { getByTestId } = render(
         <ParallaxProvider>
           <ParallaxBanner
-            className="test-class"
-            disabled={false}
             layers={[
               {
-                children: <div>test</div>,
+                children: <div data-testid="foo">foo</div>,
                 speed: 2,
               },
+              {
+                children: <div data-testid="bar">bar</div>,
+                speed: 4,
+              },
             ]}
-            style={{
-              backgroundColor: 'blue',
-              border: '1px solid red',
-            }}
           />
-        </ParallaxProvider>,
-        {
-          createNodeMock,
-        }
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+        </ParallaxProvider>
+      );
+      expect(getByTestId('foo')).toBeInTheDocument();
+      expect(getByTestId('bar')).toBeInTheDocument();
+    });
   });
 
-  it('to render without expanded margins', () => {
-    const tree = renderer
-      .create(
+  describe('with layer expanded false', () => {
+    it('then it will render without expanded styles', () => {
+      const { getByTestId } = render(
         <ParallaxProvider>
           <ParallaxBanner
             layers={[
               {
-                children: <div>test</div>,
                 speed: 2,
                 expanded: false,
               },
             ]}
           />
-        </ParallaxProvider>,
-        {
-          createNodeMock,
-        }
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+        </ParallaxProvider>
+      );
+      expect(getByTestId('layer-0').style.top).toBe('0px');
+      expect(getByTestId('layer-0').style.right).toBe('0px');
+      expect(getByTestId('layer-0').style.left).toBe('0px');
+      expect(getByTestId('layer-0').style.bottom).toBe('0px');
+      expect(getByTestId('layer-0').style.position).toBe('absolute');
+    });
   });
 
-  it('to render children', () => {
-    const node = document.createElement('div');
-
-    const child = jest.fn();
-    const Child = () => {
-      child();
-      return <div />;
-    };
-
-    ReactDOM.render(
-      <ParallaxProvider>
-        <ParallaxBanner layers={[]}>
-          <Child />
-        </ParallaxBanner>
-      </ParallaxProvider>,
-      node
-    );
-
-    expect(child).toBeCalled();
+  describe('with layer expanded', () => {
+    it('then it will render with expanded styles based on speed', () => {
+      const { getByTestId } = render(
+        <ParallaxProvider>
+          <ParallaxBanner
+            layers={[
+              {
+                speed: 2,
+              },
+            ]}
+          />
+        </ParallaxProvider>
+      );
+      expect(getByTestId('layer-0').style.top).toBe('-20px');
+      expect(getByTestId('layer-0').style.right).toBe('0px');
+      expect(getByTestId('layer-0').style.left).toBe('0px');
+      expect(getByTestId('layer-0').style.bottom).toBe('-20px');
+      expect(getByTestId('layer-0').style.position).toBe('absolute');
+    });
   });
 
-  it('to render layer children', () => {
-    const node = document.createElement('div');
-
-    const childFn = jest.fn();
-    const Child = () => {
-      childFn();
-      return <div />;
-    };
-
-    ReactDOM.render(
-      <ParallaxProvider>
-        <ParallaxBanner
-          layers={[
-            {
-              children: <Child />,
-              speed: 2,
-            },
-          ]}
-        />
-      </ParallaxProvider>,
-      node
-    );
-
-    expect(childFn).toBeCalled();
+  describe('with children', () => {
+    it('then it will render children', () => {
+      const { getByTestId } = render(
+        <ParallaxProvider>
+          <ParallaxBanner layers={[]}>
+            <div data-testid="child" />
+          </ParallaxBanner>
+        </ParallaxProvider>
+      );
+      expect(getByTestId('child')).toBeInTheDocument();
+    });
   });
 
-  it('to render layers with custom props', () => {
-    const tree = renderer
-      .create(
+  describe('with custom props', () => {
+    it('then it will render children', () => {
+      const { container, getByTestId } = render(
         <ParallaxProvider>
           <ParallaxBanner
             layers={[
@@ -154,12 +129,12 @@ describe('Expect the <ParallaxBanner> component', () => {
               },
             ]}
           />
-        </ParallaxProvider>,
-        {
-          createNodeMock,
-        }
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+        </ParallaxProvider>
+      );
+      expect(container.querySelector('.my-custom-class')).toBeInTheDocument();
+      expect(container.querySelector('#my-id')).toBeInTheDocument();
+
+      expect(getByTestId('layer-0').style.background).toBe('red');
+    });
   });
 });
