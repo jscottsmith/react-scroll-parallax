98df40f85a6922f83aaad0449ab656733ae71355
J Scott Smith <jscsmith@gmail.com>
2022-01-12 15:37:12 -0800

    chore: cleanup and test useParallax hook

--- src/hooks/useParallax.ts
+++ src/hooks/useParallax.ts
@@ -1,8 +1,8 @@
 import { CreateElementOptions, Element } from 'parallax-controller';
 import { useEffect, useRef, useState } from 'react';
 import { useVerifyController } from '../components/Parallax/hooks';
+import { getParallaxProps } from '../helpers/getParallaxProps';
 import { ParallaxProps } from '../types';
-import { removeUndefinedObjectKeys } from '../utils/removeUndefinedObjectKeys';
 import { useController } from './useController';
 
 export function useParallax<T extends HTMLElement>(props: ParallaxProps) {
@@ -11,46 +11,23 @@ export function useParallax<T extends HTMLElement>(props: ParallaxProps) {
 
   useVerifyController(controller);
 
-  function _getElementOptions(): CreateElementOptions {
-    return {
-      // @ts-expect-error
-      el: ref.current,
-      props: removeUndefinedObjectKeys({
-        speed: props.speed,
-        translateX: props.translateX,
-        translateY: props.translateY,
-        disabled: props.disabled,
-        rotate: props.rotate,
-        rotateX: props.rotateX,
-        rotateY: props.rotateY,
-        rotateZ: props.rotateZ,
-        scale: props.scale,
-        scaleX: props.scaleX,
-        scaleY: props.scaleY,
-        scaleZ: props.scaleZ,
-        opacity: props.opacity,
-        easing: props.easing,
-        rootMargin: props.rootMargin,
-        shouldStartAnimationInitialInView:
-          props.shouldStartAnimationInitialInView,
-        onProgressChange: props.onProgressChange,
-        onChange: props.onChange,
-        onEnter: props.onEnter,
-        onExit: props.onExit,
-        startScroll: props.startScroll,
-        endScroll: props.endScroll,
-        targetElement: props.targetElement,
-      }),
-    };
-  }
-
   const [element, setElement] = useState<Element>();
 
   // create element
   useEffect(() => {
-    const newElement = controller?.createElement(_getElementOptions());
-
-    setElement(newElement);
+    let newElement: Element | undefined;
+    if (ref.current instanceof HTMLElement) {
+      const options: CreateElementOptions = {
+        el: ref.current,
+        props: getParallaxProps(props),
+      };
+      newElement = controller?.createElement(options);
+      setElement(newElement);
+    } else {
+      throw new Error(
+        'You must assign the ref returned by the useParallax() hook to an HTML Element.'
+      );
+    }
 
     return () => {
       if (newElement) {
@@ -65,10 +42,8 @@ export function useParallax<T extends HTMLElement>(props: ParallaxProps) {
       if (props.disabled) {
         controller?.resetElementStyles(element);
       } else {
-        controller?.updateElementPropsById(
-          element.id,
-          _getElementOptions().props
-        );
+        const newProps = getParallaxProps(props);
+        controller?.updateElementPropsById(element.id, newProps);
       }
     }
   }, [
