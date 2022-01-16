989435645f332564b534c3b265e129dbab76d774
J Scott Smith <jscsmith@gmail.com>
2022-01-15 21:43:18 -0800

    docs: setup tailwind in docs and add examples

---
+++ documentation/src/components/beyond-css-effects/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Parallax } from 'react-scroll-parallax';
+import { BgContainer } from '../bg-container';
+
+export const BeyondCSSEffects = () => {
+  return (
+    <BgContainer>
+      <Parallax
+        translateX={['-400px', '0px']}
+        scale={[0.75, 1]}
+        rotate={[-180, 0]}
+        easing="easeInQuad"
+        className="rounded-lg bg-gray-600 bg-opacity-50"
+      >
+        <div className="border-2 border-blue-200 border-solid flex items-center justify-center bg-blue-400 h-48 w-48 rounded-lg">
+          <p className="text-center font-bold uppercase">Heyo!</p>
+        </div>
+      </Parallax>
+    </BgContainer>
+  );
+};
