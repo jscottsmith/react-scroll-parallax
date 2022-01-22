f4a6e39bfd40b02742af882d3eac362418b8bf90
J Scott Smith <jscsmith@gmail.com>
2022-01-21 20:08:28 -0800

    docs: add advanced banners doc

---
+++ documentation/src/components/advanced-banner-top/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { ParallaxBanner } from 'react-scroll-parallax';
+import { BannerLayer } from 'react-scroll-parallax/dist/components/ParallaxBanner/types';
+
+export const AdvancedBannerTop = () => {
+  const background: BannerLayer = {
+    image:
+      'https://s3-us-west-2.amazonaws.com/s.cdpn.io/105988/banner-background.jpg',
+    translateY: [0, 50],
+    opacity: [1, 0.3],
+    scale: [1.05, 1, 'easeOutCubic'],
+    shouldAlwaysCompleteAnimation: true,
+  };
+
+  const headline: BannerLayer = {
+    translateY: [0, 30],
+    scale: [1, 1.05, 'easeOutCubic'],
+    shouldAlwaysCompleteAnimation: true,
+    expanded: false,
+    children: (
+      <div className="absolute inset-0 flex items-center justify-center">
+        <h1 className="text-6xl md:text-8xl text-white font-thin">
+          Hello World!
+        </h1>
+      </div>
+    ),
+  };
+
+  const foreground: BannerLayer = {
+    image:
+      'https://s3-us-west-2.amazonaws.com/s.cdpn.io/105988/banner-foreground.png',
+    translateY: [0, 15],
+    scale: [1, 1.1, 'easeOutCubic'],
+    shouldAlwaysCompleteAnimation: true,
+  };
+
+  const gradientOverlay: BannerLayer = {
+    opacity: [0, 0.9],
+    shouldAlwaysCompleteAnimation: true,
+    expanded: false,
+    children: (
+      <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-blue-900" />
+    ),
+  };
+
+  return (
+    <>
+      <ParallaxBanner
+        layers={[background, headline, foreground, gradientOverlay]}
+        className="aspect-[2/1] bg-gray-900"
+      />
+      <figcaption className="my-md">
+        <p className="text-sm">Advanced banner configuration example.</p>
+      </figcaption>
+    </>
+  );
+};
