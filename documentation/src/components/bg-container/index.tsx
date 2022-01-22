21db14e6bba5f7ba50ff223c0bf36f9e1689d126
J Scott Smith <jscsmith@gmail.com>
2022-01-22 15:36:09 -0800

    docs: start scroll effects example doc

--- documentation/src/components/bg-container/index.tsx
+++ documentation/src/components/bg-container/index.tsx
@@ -5,7 +5,11 @@ export const BgContainer = (props: PropsWithChildren<{}>) => {
   return (
     <div className="relative my-2xl bg-gray-900 px-lg py-96 w-full rounded-md px-bg text-black overflow-hidden">
       {/* <div className="h-screen" /> */}
-      <div className="flex flex-row items-center justify-evenly w-full">
+
+      <div
+        className="flex flex-row items-center justify-evenly w-full"
+        style={{ perspective: '250px' }}
+      >
         {props.children}
       </div>
       {/* <div className="h-screen" /> */}
