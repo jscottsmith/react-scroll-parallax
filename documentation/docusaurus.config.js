a166c1f32dc9a0dea02f978d9d6461fa589285c3
J Scott Smith <jscsmith@gmail.com>
2022-01-24 19:45:23 -0800

    docs: add algolia config

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -37,6 +37,26 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      algolia: {
+        // If Algolia did not provide you any appId, use 'BH4D9OD16A'
+        appId: 'PI3J7PM083',
+
+        // Public API key: it is safe to commit it
+        apiKey: '6e7f760b010b86880a25aae88e288671',
+
+        indexName: 'react-scroll-parallax.damnthat.tv',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        //... other Algolia params
+      },
       navbar: {
         hideOnScroll: true,
         title: 'React Scroll Parallax',
