3932a21cd9cf9c484ead7c75cc0d90260853adc1
J Scott Smith <jscsmith@gmail.com>
2022-01-24 22:35:22 -0800

    docs: update night theme to `palenight`

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -2,7 +2,7 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const darkCodeTheme = require('prism-react-renderer/themes/palenight');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
