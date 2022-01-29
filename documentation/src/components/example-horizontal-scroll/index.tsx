f699c71dd204f37c2e7d288ff47a60caba02682d
J Scott Smith <jscsmith@gmail.com>
2022-01-28 23:12:29 -0800

    docs: add horizontal scroll example

---
+++ documentation/src/components/example-horizontal-scroll/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const ExampleHorizontalScroll = () => {
+  return (
+    <div className="mb-md">
+      <iframe
+        src="https://react-scroll-parallax-horizontal-scroll.surge.sh/"
+        style={{ aspectRatio: '1.5 / 1' }}
+        width="100%"
+      />
+    </div>
+  );
+};
