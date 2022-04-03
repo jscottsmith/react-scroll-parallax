1c84ec7d565c8d93063a220d9e3d7fce02ee7948
J Scott Smith <jscsmith@gmail.com>
2022-04-03 14:53:32 -0700

    feat: handle composed `ParallaxBannerLayers` as children; add story demonstration #147

--- src/components/ParallaxBanner/index.tsx
+++ src/components/ParallaxBanner/index.tsx
@@ -4,6 +4,7 @@ import React, {
   useEffect,
   useRef,
   useState,
+  ReactElement,
 } from 'react';
 import { ParallaxBannerLayer } from './components/ParallaxBannerLayer';
 import { ParallaxBannerProps } from './types';
@@ -17,9 +18,8 @@ const containerStyle: CSSProperties = {
 export const ParallaxBanner = (
   props: PropsWithChildren<ParallaxBannerProps>
 ) => {
-  const [targetElement, setTargetElement] = useState<HTMLDivElement | null>(
-    null
-  );
+  const [targetElement, setTargetElement] =
+    useState<HTMLDivElement | null>(null);
   const containerRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     setTargetElement(containerRef.current);
@@ -30,24 +30,52 @@ export const ParallaxBanner = (
     layers = [],
     ...rootRest
   } = props;
+
+  function renderLayers() {
+    if (targetElement) {
+      const shouldUseLayers = layers && layers.length > 0;
+      if (shouldUseLayers) {
+        return layers.map((layer, i) => (
+          <ParallaxBannerLayer
+            {...layer}
+            targetElement={targetElement}
+            key={`layer-${i}`}
+            testId={`layer-${i}`}
+          />
+        ));
+      }
+    }
+    return null;
+  }
+
+  function renderChildren() {
+    if (targetElement) {
+      return React.Children.map(props.children, (child) => {
+        const item = child as ReactElement<
+          PropsWithChildren<{ targetElement: any }>
+        >;
+        // adds the targetElement prop to any ParallaxBannerLayer components
+        if (item.type === ParallaxBannerLayer) {
+          const clone = React.cloneElement(item, {
+            targetElement,
+          });
+          return clone;
+        }
+        return child;
+      });
+    }
+    return null;
+  }
   return (
     <div
       ref={containerRef}
       style={{ ...containerStyle, ...rootStyle }}
       {...rootRest}
     >
-      {layers.map(
-        (layer, i) =>
-          targetElement && (
-            <ParallaxBannerLayer
-              {...layer}
-              targetElement={targetElement}
-              key={`layer-${i}`}
-              testId={`layer-${i}`}
-            />
-          )
-      )}
-      {props.children}
+      {/* Using the `layers` prop to define children */}
+      {renderLayers()}
+      {/* Using children to compose layers */}
+      {renderChildren()}
     </div>
   );
 };
