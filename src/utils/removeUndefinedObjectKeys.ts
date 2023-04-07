7cf31a84e18341c72d9450e22eccd95fbd794587
Dimension <to.hitesh.gorana@gmail.com>
2023-04-07 11:03:40 +0530

    chore: refactor removeUndefinedObjectKeys function (#191)
    Co-authored-by: Hitesh <hitesh.g@smarter.codes>

--- src/utils/removeUndefinedObjectKeys.ts
+++ src/utils/removeUndefinedObjectKeys.ts
@@ -1,6 +1,6 @@
 export function removeUndefinedObjectKeys(obj: { [key: string]: any }) {
   Object.keys(obj).forEach((key) =>
-    obj[key] === undefined ? delete obj[key] : {}
+    obj[key] === undefined && delete obj[key]
   );
   return obj;
 }
