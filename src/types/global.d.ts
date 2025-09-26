f7480619fcf144f21df17d8253eb889a4342d56e
J Scott Smith <jscsmith@gmail.com>
2025-09-26 15:52:11 -0700

    fix: build

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,3 +2,16 @@
 declare global {
   const global: any;
 }
+
+// Import jest-dom types
+import '@testing-library/jest-dom';
+
+// Extend Jest matchers
+declare module '@jest/expect' {
+  interface Matchers<R> {
+    toBeInTheDocument(): R;
+    toHaveAttribute(attr: string, value?: string): R;
+    toHaveClass(className: string): R;
+    toHaveStyle(css: string | Record<string, any>): R;
+  }
+}
