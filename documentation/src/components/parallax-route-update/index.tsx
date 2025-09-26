d45f6ef093a0516b28b375ef9f68397d753d4b54
J Scott Smith <jscsmith@gmail.com>
2025-09-26 13:42:59 -0700

    fix: jest ts

--- documentation/src/components/parallax-route-update/index.tsx
+++ documentation/src/components/parallax-route-update/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation } from '@docusaurus/router';
 import { useParallaxController } from 'react-scroll-parallax';
 
 function useUpdateControllerOnRouteChange() {
--- jest-setup.ts
+++ jest-setup.ts
@@ -1,1 +1,1 @@
-import '@testing-library/jest-dom';
+require('@testing-library/jest-dom');
