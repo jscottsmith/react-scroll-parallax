feb63370a85db67ffdc12a7dd70b6da25dfe7b72
J Scott Smith <jscsmith@gmail.com>
2026-05-13 14:02:57 -0700

    chore: update dev deps

--- tsdown.config.js
+++ tsdown.config.js
@@ -1,12 +1,16 @@
-export default {
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
   entry: 'src/index.ts',
   outDir: 'dist',
   format: ['esm', 'cjs'],
   dts: true,
-  external: ['react', 'react-dom'],
+  deps: {
+    neverBundle: ['react', 'react-dom'],
+  },
   clean: true,
   sourcemap: true,
   minify: false,
   splitting: false,
   treeshake: true,
-};
+});
