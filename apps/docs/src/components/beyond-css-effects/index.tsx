ad9299cfbaac71c45c2b55de7634cde509d0dd29
J Scott Smith <jscsmith@gmail.com>
2026-05-16 00:04:03 -0700

    fix: doc examples

--- apps/docs/src/components/beyond-css-effects/index.tsx
+++ apps/docs/src/components/beyond-css-effects/index.tsx
@@ -9,7 +9,7 @@ export const BeyondCSSEffects = () => {
         translateX={['-400px', '0px']}
         scale={[0.75, 1]}
         rotate={[-180, 0]}
-        easing="easeInQuad"
+        easing="cubic-bezier(0.55, 0.085, 0.68, 0.53)"
         className="rounded-lg bg-gray-600 bg-opacity-50"
         shouldAlwaysCompleteAnimation
       >
--- apps/docs/src/components/bg-container/index.tsx
+++ apps/docs/src/components/bg-container/index.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react';
 
 export const BgContainer = (props: PropsWithChildren<{}>) => {
   return (
-    <div className="relative my-2xl bg-gray-900 px-lg py-96 w-full rounded-md px-bg text-black overflow-hidden">
+    <div className="relative my-2xl bg-gray-900 px-lg py-96 w-full rounded-md px-bg text-black overflow-clip">
       {/* <div className="h-screen" /> */}
 
       <div
