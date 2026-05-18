dd1edd1a46ed33d2f971ec865a64f2de78abb62d
J Scott Smith <jscsmith@gmail.com>
2026-05-18 16:09:59 -0700

    chore: removed root margin; update stories and examples

--- src/testUtils/tests.constants.ts
+++ src/testUtils/tests.constants.ts
@@ -23,6 +23,5 @@ export const ALL_PARALLAX_PROPS = [
   { onExit: () => {} },
   { onChange: () => {} },
   { onProgressChange: () => {} },
-  { rootMargin: { top: 0, right: 0, bottom: 0, left: 0 } },
   { targetElement: document.createElement('div') },
 ];
